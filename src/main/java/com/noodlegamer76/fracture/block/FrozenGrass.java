ee9bef6e0e987e0c3435a0209e22bb532fc4dca6
Noodlegamer76 <ab09814j@gmail.com>
2024-11-24 13:44:53 -0700

    added some stuff

--- src/main/java/com/noodlegamer76/fracture/block/FrozenGrass.java
+++ src/main/java/com/noodlegamer76/fracture/block/FrozenGrass.java
@@ -2,6 +2,8 @@ package com.noodlegamer76.fracture.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 
@@ -11,7 +13,10 @@ public class FrozenGrass extends SnowPlant {
     }
 
     @Override
-    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity) {
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (pEntity.getType() == EntityType.ITEM) {
+            return;
+        }
         pLevel.destroyBlock(pPos, true, pEntity);
     }
 }
